0f03b2a508bb869628d653abc93bd1d7a0d6f2ca
David <dl01852@georgiasouthern.edu>
2015-11-18 21:47:12 -0500

    State class is created, more functionality will be added as needed

--- src/State.java
+++ src/State.java
@@ -1,9 +1,47 @@
-
+import java.util.HashMap;
+import java.util.Map;
 public class State {
 
-	public static void main(String[] args) {
+    private String valueBeingRead;
+	private State nextState;
+    private State currentState; // not quite sure how this will work or if i'll need this.
+	private Map<String, State> transitionMap = new HashMap<>(); // gotta figure out how to populate this Mapping.
+    private boolean acceptState;
+    private boolean startState;
 
-		System.out.println("hello world");
-	}
+    // get properties for each field.
+    public String getValueBeingRead(){return this.valueBeingRead;}
+    public boolean  getAcceptState() {return this.acceptState;}
+    public State  getNextState() { return this.nextState;}
+    public boolean getStartState() { return this.startState;}
+    public Map getTransitionMap() {return this.transitionMap;}
+
+    // set properties for each field.
+    public void ValueBeingRead(String valueToRead){this.valueBeingRead = valueToRead;}
+    public void setAcceptState(boolean acceptState){this.acceptState = acceptState;}
+    public void setNextState(){} // not sure how this function will work just yet.
+    public void setStartState(boolean startState){this.startState = startState;}
+
+    // constructor
+    public State(boolean startState, boolean acceptState)
+    {
+        setStartState(startState);
+        setAcceptState(acceptState);
+    }
+
+    public State Transition(State currentState, String currentInput) // method takes in the CurrentState it is currently on and the input being read.
+    {                                                                // I think the currentState can be taken out cause the State calling this
+                                                                    // method *SHOULD* be the currentState(so you can essentially use 'this' keyword).
+        Map<String,State> transitionState = currentState.getTransitionMap();
+        nextState = transitionState.get(currentInput); // grabs the next state based on the input.
+        return nextState;
+    }
+
+    public void displayinfo() // Might not need this.
+    {
+
+    }
+
 
 }
+

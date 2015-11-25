dc993b70f7659ecdc99637b1e76aa871da319a19
jlarkin21 <jlarkin9@gmail.com>
2015-11-25 18:02:04 -0500

    …

--- src/State.java
+++ src/State.java
@@ -1,5 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.*;
+
 
 // All this class does is handle everything about each individual State
 // and all the things a STATE should be worried about.
@@ -74,5 +80,67 @@ public class State {
     }
 
 
-}
+
+        public static void main(String[] args) throws IOException {
+            FileInputStream in = new FileInputStream("/Users/jlarkin9/Desktop/test.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            String strLine = null;
+
+            String[] filearray;
+            filearray = new String[12];
+
+            while ((filearray[0] = br.readLine()) != null) {
+                System.out.println("DFA:\n" + filearray[0]);
+
+
+                for (int j = 1; j < filearray.length; j++){
+                    filearray[j] = br.readLine();
+                    System.out.println(filearray[j]);
+                }
+
+                filearray[0] = filearray[0].substring(1, filearray[0].indexOf('}'));
+                filearray[1] = filearray[1].substring(1, filearray[1].indexOf('}'));
+                filearray[3] = filearray[3].substring(1, filearray[3].indexOf('}'));
+
+                String[] alphabet = filearray[0].split(",");
+                String[] states = filearray[1].split(",");
+                String start = filearray[2];
+                String accept = filearray[3];
+
+                System.out.println("alphabet: " + alphabet[0] + " " + alphabet[1]);
+                System.out.print("states: ");
+                for (int i = 0; i < states.length;i++) {
+                    System.out.print(states[i] + " ");
+                }
+                System.out.println("\nstart state: " + start);
+                System.out.println("accept state: " + accept);
+
+                String[][] transitionMap;
+                int transitionsNum = (states.length * alphabet.length);
+                System.out.println(transitionsNum);
+                transitionMap = new String[transitionsNum][2];
+                for(int i = 0, j = 4; i < transitionsNum && j < filearray.length;i++,j++) {
+                    transitionMap[i] = filearray[j].split("->");
+                }
+                for (int k = 0; k < transitionsNum;k++){
+                    for(int l = 0; l < 2;l++){
+                        System.out.print(transitionMap[k][l] + " ");
+                    }
+                    System.out.print("\n");
+                }
+
+            }
+            in.close();
+
+        }
+    }
+
+
+
+
+
+
+
+
 
